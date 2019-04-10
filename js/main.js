e7be645d36f750e997bdae25fde843af39a56667
Mostafa <mos.sameti@gmail.com>
2019-04-11 01:07:51 +0430

    add change number of rounds feature

--- js/main.js
+++ js/main.js
@@ -1,58 +1,76 @@
 "use strict";
 
-var activityTime, shortBreakTime, longBreakTime;
-var activeCounter = 1, shortBreakCounter = 0 , longBreakCounter = 1;
+var activityTime, shortBreakTime, longBreakTime, numberOfRounds;
+var activeCounter = 1,
+  shortBreakCounter = 0,
+  longBreakCounter = 1;
 var startTimer;
 
 function setTimerAndStart() {
-  activityTime = (document.forms.settingsForm.active.value * 60000);
-  shortBreakTime = (document.forms.settingsForm.shortBreak.value * 60000);
-  longBreakTime = (document.forms.settingsForm.longBreak.value * 60000);
+  activityTime = document.forms.settingsForm.active.value * 60000;
+  shortBreakTime = document.forms.settingsForm.shortBreak.value * 60000;
+  longBreakTime = document.forms.settingsForm.longBreak.value * 60000;
+  numberOfRounds = document.forms.settingsForm.rounds.value;
   intervalSetter(activityTime);
 }
 
 function resetTimer() {
   clearInterval(startTimer);
-  document.getElementById("remainingTime").innerHTML = "--:--"
+  document.getElementById("remainingTime").innerHTML = "--:--";
 }
 
 function showSliderValue(event) {
-  document.getElementById(event.target.name + "TimeValue").innerHTML = event.target.value + ":00";
-}
-
-function whatUserMustDo() {
-  if (activeCounter / 4 === longBreakCounter) { //true means user need a long break
-    longBreakCounter++ ;
-    shortBreakCounter++ ;
-    intervalSetter(longBreakTime);
-    return;
-  } else if (activeCounter === shortBreakCounter) { //true means user need an activity
-    activeCounter++ ;
-    intervalSetter(activityTime);
-    return;
-  } else if(activeCounter !== shortBreakCounter) { //true means user need a short break
-    shortBreakCounter++ ;
-    intervalSetter(shortBreakTime);
-    return;
+  try {
+    document.getElementById(event.target.name + "TimeValue").innerHTML =
+    event.target.value + ":00";
+  } catch (error) {
+    document.getElementById(event.target.name + "Value").innerHTML =
+    event.target.value;
   }
 }
 
-function intervalSetter(time) { //showimg remaining time every 1 second
-  var startTime = Number(new Date());
-  clearInterval(startTimer);
-  startTimer = setInterval(function() { remainingTime(time + startTime) }, 1000);
+function whatUserMustDo() {
+  if (activeCounter / numberOfRounds === longBreakCounter) {
+    //true means user need a long break
+    longBreakCounter++;
+    shortBreakCounter++;
+    return intervalSetter(longBreakTime);
+  } else if (activeCounter === shortBreakCounter) {
+    //true means user need an activity
+    activeCounter++;
+    return intervalSetter(activityTime);
+  } else if (activeCounter !== shortBreakCounter) {
+    //true means user need a short break
+    shortBreakCounter++;
+    return intervalSetter(shortBreakTime);
+  }
 }
 
-function remainingTime(endTime) { //calculating remaining time of activity, short and long break
+function intervalSetter(time) {
+  //showimg remaining time every 1 second
+  var startTime = Number(new Date());
+  clearInterval(startTimer);
+  startTimer = setInterval(function() {
+    remainingTime(time + startTime);
+  }, 1000);
+}
+
+function remainingTime(endTime) {
+  //calculating remaining time of activity, short and long break
   var currentTime = Number(new Date());
-  remainedMilliSeconds = endTime - currentTime;
-  remainingSeconds = ((remainedMilliSeconds % 60000) / 1000).toFixed(0);
-  remainingMinutes = Math.floor(remainedMilliSeconds / 60000);
+  var remainedMilliSeconds = endTime - currentTime;
+  var remainingSeconds = ((remainedMilliSeconds % 60000) / 1000).toFixed(0);
+  var remainingMinutes = Math.floor(remainedMilliSeconds / 60000);
   if (remainedMilliSeconds < 0) {
     whatUserMustDo();
     remainingSeconds = 0;
     remainingMinutes = 0;
   }
-  document.getElementById("remainingTime").innerHTML = 
-  remainingSeconds == 60 ? (remainingMinutes+1) + ":00" : remainingMinutes + ":" + (remainingSeconds < 10 ? '0' : '') + remainingSeconds;
+  document.getElementById("remainingTime").innerHTML =
+    remainingSeconds == 60
+      ? remainingMinutes + 1 + ":00"
+      : remainingMinutes +
+        ":" +
+        (remainingSeconds < 10 ? "0" : "") +
+        remainingSeconds;
 }
